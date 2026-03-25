7e7b2bfe3f1fc06a26c5750ad00069bdf51f22dd
wnstkd704 <wnstkd704@gamil.com>
2026-03-25 09:58:35 +0900

    테스트

--- src/main/java/com/beyond/configserver/Application.java
+++ src/main/java/com/beyond/configserver/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer // 스프링 클라우드 컨피그 서버로 활성화 한다.
+@EnableConfigServer // 스프링 클라우드 컨피그 서버로 활성화 한다
 @SpringBootApplication
 public class Application {
 
