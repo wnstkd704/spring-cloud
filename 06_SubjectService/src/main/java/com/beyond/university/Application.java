2266ffd90b91e5466d612cf84dafd60783b639a2
wnstkd704 <wnstkd704@gamil.com>
2026-03-26 12:41:34 +0900

    스프링 클라우드 게이트웨이 파일 추가

--- 06_SubjectService/src/main/java/com/beyond/university/Application.java
+++ 06_SubjectService/src/main/java/com/beyond/university/Application.java
@@ -2,6 +2,7 @@ package com.beyond.university;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ public class Application {
 	}
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		// Spring에서 제공하는 HTTP 통신을 위한 템플릿 클래스이다.
 		return new RestTemplate();
